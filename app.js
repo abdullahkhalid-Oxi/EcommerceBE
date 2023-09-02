f4d6b617a10dbbb355562ed7d1356799010c41ae
Your Name <you@example.com>
2023-09-02 16:37:38 +0500

    0

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "./client/dist")));
 
 app.get("*", function (_, res) {
   res.sendFile(path.join(__dirname, "./client/dist/index.html"), function (err) {
